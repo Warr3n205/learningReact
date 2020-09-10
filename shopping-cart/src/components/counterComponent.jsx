45c9dabd08369c7f398682c2e873466baae3c2b9
Warr3n205 <68779758+Warr3n205@users.noreply.github.com>
2020-09-10 17:26:28 +0800

    Update counterComponent.jsx

--- shopping-cart/src/components/counterComponent.jsx
+++ shopping-cart/src/components/counterComponent.jsx
@@ -17,13 +17,36 @@ class Counter extends Component {
     );
   }
 
+  handleIncrement() {
+    console.log("Increment Clcik");
+  }
+
   render() {
     return (
       <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+
         {this.state.tags.length === 0 && "Please create a new tag!"}
         {this.renderTags()}
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
